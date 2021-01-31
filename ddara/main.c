be9c4dbf160d65592dc53149c88fd2e1accaa7e5
Curduks <dmavk12@naver.com>
2021-02-01 00:26:43 +0900

    모든 자릿수 더하기

--- ddara/main.c
+++ ddara/main.c
@@ -7,10 +7,25 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    printf("Heeloasddasda\n");
-    printf("왜 안dasdsdddd돼\n");
-    return 0;
+void plus(int);
+
+int main(int argc, const char * argv[])
+{
+    
+    plus(312);
+    plus(13212);
+}
+
+void plus(int n)
+{
+    int c = n;
+    int result = 0;
+    
+    while(n>0)
+    {
+        result += n%10;
+        n = n/10;
+    }
+    
+    printf("%d의 자릿수 합은: %d 입니다.\n",c,result);
 }
