fa09ba59c607b14dada9a09a6bb23a64096c6249
Curduks <dmavk12@naver.com>
2021-01-31 16:26:22 +0900

    das

--- ddara/main.c
+++ ddara/main.c
@@ -10,6 +10,7 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
-    printf("Heeloasddasda");
+    printf("Heeloasddasda\n");
+    printf("왜 안돼\n");
     return 0;
 }
