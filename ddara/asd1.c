d6ec46c29a981a78aaf6be79c2284858481275bf
Curduks <dmavk12@naver.com>
2021-02-07 22:13:03 +0900

    피라미드

--- ddara/asd1.c
+++ ddara/asd1.c
@@ -7,14 +7,41 @@
 
 #include <stdio.h>
 
-void sum(int);
+void spira(int);
+void nrpira(int);
 
 int main(int argc, const char * argv[])
 {
-    for(int i =1; i<6;i++)
+    int a;
+    printf("숫자 입력: "); scanf("%d", &a);
+    nrpira(a);
+}
+
+
+
+void spira(int n)
+{
+    
+    for (int i=1; i<=n; i++)
     {
-        for(int p=6; p>i;p--)
-            printf("*");
-        printf("\n");
+        for(int k=n; k>i ; k--)
+            putchar(' ');
+        for(int j=0; j<(i-1)*2+1; j++)
+            putchar('*');
+        putchar('\n');
+    }
+    
+}
+
+void nrpira(int n)
+{
+    for(int i=1; i<=n; i++)
+    {
+        for(int k=0;k<i;k++)
+            printf(" ");
+        for(int j=(n-1)*2+1; j>=(i-1)*2+1 ; j--)
+            printf("%d",i);
+
+        putchar('\n');
     }
 }
